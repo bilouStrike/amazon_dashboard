6ee377f078cfc0bff68e5f4977edc0eeda11d66b
bilal <bilal89dif@gmail.com>
2020-07-29 08:58:53 +0200

    add form example

--- src/components/Sales/addMarketplace.js
+++ src/components/Sales/addMarketplace.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import { Button, Form, Input } from 'antd';
+import React,{useState} from 'react';
+import { Button, Input, Col, Row, Form, Card } from 'antd';
 import FullScreenModel from 'components/FullScreenModel';
-
-const FormItem = Form.Item;
 
 const AddMarketplace = () =>  {
 
-  const handle = async (values) => {
-    
-  }
+    const [expand, setExpand] = useState(false);
+    const [form] = Form.useForm();
+  
+    const getFields = () => {
+      const count = 12;
+      const children = [];
+      for (let i = 0; i < count; i++) {
+        children.push(
+          <Col lg={8} md={8} sm={12} xs={24} key={i} style={{display: i < count ? 'block' : 'none'}}>
+            <div className="gx-form-row0">
+              <Form.Item
+                name={`field-${i}`}
+                label={`Field ${i}`}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Input something!',
+                  },
+                ]}
+              >
+                <Input/>
+              </Form.Item>
+            </div>
+          </Col>,
+        );
+      }
+      return children;
+    };
+  
+    const onFinish = values => {
+      console.log('Received values of form: ', values);
+    };
 
   return (
     <>
@@ -17,32 +44,30 @@ const AddMarketplace = () =>  {
             buttonTitle="+ Add Marketplace"
             title="Add Marketplace"
         >
-            <Form 
-                initialValues={{ remember: true }}
-                name="Add Marketplace"
-                onFinish={handle}
-            >
-                <Form.Item
-                    name="name"
-                    label="Name"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input the marketplace name!',
-                        },
-                    ]}
+           <Card className="gx-card" title="Add Marketplace">
+                <Form
+                    form={form}
+                    name="Add Marketplace"
+                    className="ant-advanced-search-form"
+                    onFinish={onFinish}
                 >
-                    <Input />
-                </Form.Item>
-                <FormItem>
-                    <Button className="gx-mb-0"
-                        type="primary"
-                        htmlType="submit"
-                    >
-                        ADD
-                    </Button>
-                </FormItem>
-            </Form>
+                    <Row gutter={24}>{getFields()}</Row>
+                    <Row>
+                        <Col span={24} style={{ textAlign: 'right' }}>
+                        <Button type="primary" htmlType="submit">
+                            Search
+                        </Button>
+                        <Button
+                            onClick={() => {
+                            form.resetFields();
+                            }}
+                        >
+                            Clear
+                        </Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Card>
         </FullScreenModel>
     </>
     );
